6e941897f7e65e444e875a35eb6a5a3733affb59
codesmurf <blackramau@gmail.com>
2021-06-11 16:28:00 +0930

    host is now barebones

--- echo/install-android.sh
+++ echo/install-android.sh
@@ -1,5 +1,8 @@
+ [ -d 'host' ] && rm -r host
+ cordova create host au.com.rs.host Host
  cd host
  cordova platform add android
+ git checkout www
  cordova plugin add --link ../plugin/EchoPlugin
  cordova build android
  cordova requirements
